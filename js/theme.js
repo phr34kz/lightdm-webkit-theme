61f748909abd4a1486a549f93d533f735ae3a6e9
Erik <erik.edrosa@gmail.com>
2012-10-20 19:54:27 -0400

    Added hostname as brand and session options

--- js/theme.js
+++ js/theme.js
@@ -2,5 +2,20 @@
  * Onload startup
  */
 $(document).ready(function() {
+	
+	//set hostname for brand
+	$("#hostname").text(lightdm.hostname);
+
+	//create a list of session options
+	var sessions = [];
+
+	for ( i in lightdm.sessions ) {
+
+		var session = lightdm.sessions[i];
+		sessions.push( $("<option></option>").text(session) );
+
+	}
+
+	$("#session").append(sessions);
 
 }

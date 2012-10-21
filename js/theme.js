b44c4c46126c141a900d3da6365a985cd7b4ec0c
Erik <erik.edrosa@gmail.com>
2012-10-21 00:20:43 -0400

    fixed javascript and added a log

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	
 	//set hostname for brand
 	$("#hostname").text(lightdm.hostname);
+	log("hostname: " + lightdm.hostname);
 
 	//create a list of session options
 	var sessions = [];
@@ -12,10 +13,59 @@ $(document).ready(function() {
 	for ( i in lightdm.sessions ) {
 
 		var session = lightdm.sessions[i];
-		sessions.push( $("<option></option>").text(session) );
+		log("session: " + session.name);
+		sessions.push( $("<option></option>").text(session.name) );
 
 	}
 
 	$("#session").append(sessions);
 
+
+
+	$("#logIn").click( function() {
+		log("Log in button clicked");
+		selectedUser = $("#inputUsername").val() );
+		log("user: " + selectedUser);
+		lightdm.start_authentication( selectedUser );
+		submitPassword();
+	});
+
+
+	if(DEBUG) {
+		$("#log").show();
+
+	}
+
+});
+
+function submitPassword(){
+	log("submitPassword called");
+
+	lightdm.provide_secret($("#inputPassword").val());
+}
+
+function authentication_complete() {
+	log("authentication_complete");
+	if( lightdm.is_authenticated ) {
+		
+		lightdm.login( lightdm.authentication_user, $("#session").val());
+
+	}else {
+		//Authentication Failed
+		log("authentication failed");
+	}
+
+}
+
+function show_prompt(text) {
+	log("show_prompt called");
+}
+
+var DEBUG = true;
+var selectedUser = null;
+
+function log(text) {
+	if(DEBUG) {
+		$("#log").append(text);
+		$("#log").append("<br/>");
 }
